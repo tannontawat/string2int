953417166897a963cccab7660ef0e539856e8ab1
Nontawat Tantavanich <tbn_nontawat@hotmail.com>
2021-02-22 23:09:46 +0700

    Init challenge

---
+++ StringToInt/StringToInt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Char;
+
+namespace StringToInt
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter value >> ");
+                var originalString = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(originalString))
+                {
+                    var num = 0;
+                    foreach (var character in originalString)
+                    {
+                        if (IsDigit(character))
+                        {
+                            // From ASCII value of '0' is 48.
+                            // I multiple with 10 for push the digit to next index.
+                            num = num * 10 + (character - 48);
+                        }
+                    }
+                    Console.WriteLine(num != 0 ? $"The number is {num}" : "Not have any number.");
+                    Console.WriteLine("Thank you");
+                    return;
+                }
+            }
+        }
+    }
+}
